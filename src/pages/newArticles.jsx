7ca1f8af873874c90f548b79d3a2e40acefcb1d5
github user mura012 <murakoudai@gmail.com>
2022-07-10 21:18:59 +0900

    ✨ ファイルを追加

---
+++ src/pages/newArticles.jsx
@@ -0,0 +1,11 @@
+import Header from "src/components/Header";
+
+const Articles = () => {
+  return (
+    <div>
+      <Header />
+      <h1>新着記事</h1>
+    </div>
+  );
+};
+export default Articles;
