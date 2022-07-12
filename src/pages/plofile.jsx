dd3a142d8af4778d23dfe8bef25c42c8f1ba0eb7
github user mura012 <murakoudai@gmail.com>
2022-07-13 08:37:57 +0900

    とりあえずプロフィールを記載

--- src/pages/plofile.jsx
+++ src/pages/plofile.jsx
@@ -1,10 +1,23 @@
+import Footer from "src/components/footer/Footer";
 import Header from "src/components/Header";
 
 const Profile = () => {
   return (
     <>
       <Header />
-      <p>hello</p>
+      <h1>武良</h1>
+      <h1>MURA</h1>
+      <p>フロントエンドエンジニアになるために勉強中</p>
+      <h2>生年月日</h2>
+      <p>2001年8月28日生まれ(20歳)</p>
+      <h2>趣味</h2>
+      <h4>運動、漫画、お笑い鑑賞</h4>
+      <p>
+        勉強していない時間はだいたいこのどれかをしています。
+        <br />
+        ゲームは得意でないのであまりしません。
+      </p>
+      <Footer />
     </>
   );
 };
