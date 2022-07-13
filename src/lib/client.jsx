734e58050fe6a4119cd78a071127ad0c15e5a5d9
github user mura012 <murakoudai@gmail.com>
2022-07-13 20:26:52 +0900

    aoiKeyを隠す

--- src/lib/client.jsx
+++ src/lib/client.jsx
@@ -3,5 +3,5 @@ import { createClient } from "microcms-js-sdk"; //ES6
 // Initialize Client SDK.
 export const client = createClient({
   serviceDomain: "mura-blog", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: "f08a795e2dd84b33ae665cc0fd162be8909a",
+  apiKey: process.env.API_KEY,
 });
