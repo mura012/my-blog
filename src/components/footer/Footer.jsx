ba183650f7b8767b66dd2a0215db8772b7eb325d
github user mura012 <murakoudai@gmail.com>
2022-07-14 21:46:08 +0900

    ✨ フッターを作成

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,21 @@
+import classes from "./Footer.module.css";
+
 export const Footer = () => {
-  return <p>©</p>;
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.footerWrapper}>
+        <p className={classes.question}>
+          何かあれば
+          <a
+            href="https://twitter.com/most_love08"
+            className={classes.twitterLink}
+          >
+            ツイッターのDM
+          </a>
+          にお願いします。
+        </p>
+        <p className={classes.copyright}>©武良</p>
+      </div>
+    </footer>
+  );
 };
