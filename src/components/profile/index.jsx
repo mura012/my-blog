0c0b0bff36f273cd6531a55a965b222d5f93de96
github user mura012 <murakoudai@gmail.com>
2022-07-14 21:47:24 +0900

    プロフィールを追記

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -7,23 +7,18 @@ export const MyProfile = () => {
       <p className={classes.alphabet}>MURA</p>
       <p className={classes.job}>フロントエンドエンジニアになるために勉強中</p>
       <h2 className={classes.h2}>生年月日</h2>
-      <p>2001年8月28日生まれ(20歳)</p>
-      <h2 className={classes.h2}>出身</h2>
-      <p>鳥取県境港市</p>
+      <p className={classes.text}>2001年8月28日生まれ(20歳)</p>
+      <h2 className={classes.h2}>出身地</h2>
+      <p className={classes.text}>鳥取県境港市</p>
       <h2 className={classes.h2}>技術スタック（レベル０）</h2>
       <ol className={classes.ol}>
-        <li>▶HTML</li>
-        <li>▶CSS</li>
-        <li>▶JavaScript</li>
-        <li>▶React(Next.js)</li>
+        <li className={classes.text}>▶HTML</li>
+        <li className={classes.text}>▶CSS</li>
+        <li className={classes.text}>▶JavaScript</li>
+        <li className={classes.text}>▶React(Next.js)</li>
       </ol>
       <h2 className={classes.h2}>趣味</h2>
-      <h4>運動、漫画、お笑い鑑賞</h4>
-      <p>
-        勉強していない時間はだいたいこのどれかをしています。
-        <br />
-        ゲームは得意でないのであまりしません。
-      </p>
+      <p className={classes.text}>運動、漫画、お笑い鑑賞</p>
     </div>
   );
 };
