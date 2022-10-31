bd8d29eb983e5418f60dc63ebfdbf451980a02ae
github user mura012 <murakoudai@gmail.com>
2022-10-31 22:02:31 +0900

    不要なdivタグを削除、Linkタグを使用

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,22 +1,18 @@
+import Link from "next/link";
 import classes from "./Footer.module.css";
 
 export const Footer = () => {
   const date = new Date();
   return (
     <footer className={classes.footer}>
-      <div className={classes.footerWrapper}>
-        <p className={classes.question}>
-          連絡は
-          <a
-            href="https://twitter.com/most_love08"
-            className={classes.twitterLink}
-          >
-            ツイッターのDM
-          </a>
-          にお願いします。
-        </p>
-        <p className={classes.copyright}>© {date.getFullYear()} 武良</p>
-      </div>
+      <p className={classes.question}>
+        連絡は
+        <Link href="https://twitter.com/most_love08">
+          <a className={classes.twitterLink}>ツイッターのDM</a>
+        </Link>
+        にお願いします。
+      </p>
+      <p className={classes.copyright}>© {date.getFullYear()} 武良</p>
     </footer>
   );
 };
